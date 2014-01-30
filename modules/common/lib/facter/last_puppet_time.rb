b2b1c7a290db687e62e66b43a38be83b4f5a0d4b
adam <adam@puppetlabs.vm>
2014-01-30 17:10:14 +0000

    cleaned up fact

--- modules/common/lib/facter/last_puppet_time.rb
+++ modules/common/lib/facter/last_puppet_time.rb
@@ -3,9 +3,7 @@ require 'puppet'
 Facter.add('last_puppet_time') do
   setcode do
     report = Puppet.settings[:lastrunreport]
-    #report
     ymf = YAML::load_file(report)
     ymf.time
-
   end
 end
